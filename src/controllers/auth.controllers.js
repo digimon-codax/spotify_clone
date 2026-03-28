9d8d5cbe835e1f48348fa7b71ecc5c2bd19ab4d1
digimon-codax <chatterjeeanish49@gmail.com>
2026-03-28 22:47:39 +0530

    feat: implement user registration controller with JWT authentication and cookie storage

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
 
 async function registerUser(req, res){
     
@@ -6,5 +7,40 @@ async function registerUser(req, res){
 
     const userExist = await userModel.findOne({
 
+        $or: [
+            {username},
+            {email}
+        ]
+
+    })
+
+    if(userExist){
+        return res.status(409).json({
+            message:"user already exists"
+        })
+    }
+
+    const user = await userModel.create({
+        username,
+        email,
+        password,
+        role
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+       
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "user registered successfully",
+        user:{
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
     })
 }
