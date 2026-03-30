55a8897502d16910c62fe0798ae8077c2be3e4af
digimon-codax <chatterjeeanish49@gmail.com>
2026-03-30 21:13:46 +0530

    feat: implement createMusic controller with artist role authorization

--- src/controllers/music.controllers.js
+++ src/controllers/music.controllers.js
@@ -1,6 +1,32 @@
 const musicModel = require("../models/music.model")
+const jwt = require("jsonwebtoken")
 
 async function createMusic(req, res){
+
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"unauthorized"
+        })
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(decoded.role !== "artist"){
+            return res.status(403).json({
+                message: "you don't have access to create a music"
+            })
+        }
+    }catch(err){
+            return res.status(401).json({
+                message: "unauthorized"
+            })
+        }
+        const {title} = req.body
+        const uri = req.file.path
+    
+    
 
 }
 
