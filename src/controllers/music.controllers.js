952a69f25aacd2fb74b7c039e569afc22574edc8
digimon-codax <chatterjeeanish49@gmail.com>
2026-05-03 20:09:36 +0530

    remove authentication checks from addMusicToAlbum function

--- src/controllers/music.controllers.js
+++ src/controllers/music.controllers.js
@@ -30,17 +30,7 @@ async function addMusic(req, res){
 }
 
 async function addMusicToAlbum(req, res){
-  const token = req.cookies.token
-
-  if(!token){
-    return res.status(401).json({message: 'Unauthorized'})
-  }
-  try{
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-    if(decoded.role !== 'artist'){
-      return res.status(403).json({message: "You don't have access to add music to an album"})
-    }
+  
 
     const {title, musicIds} = req.body
     const album = await albumModel.create({
@@ -58,10 +48,7 @@ async function addMusicToAlbum(req, res){
         musics: album.musics
       }
     })
-  }catch(err){
-    console.log(err)
-    return res.status(401).json({message: "Unauthorized"})
-  }
+  
 }
 
 module.exports = { addMusic, addMusicToAlbum }
